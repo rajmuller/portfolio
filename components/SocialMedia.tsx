907fc3af42bab68e822b393e9ecc5b11544cc288
adamRajmuller <rajfta@gmail.com>
2022-02-25 20:15:48 +0100

    social media

--- components/SocialMedia.tsx
+++ components/SocialMedia.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import { IconType } from 'react-icons';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { BsTwitter } from 'react-icons/bs';
+import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
 
-const Instance = ({ Icon }: { Icon: IconType }) => (
-  <div className="my-1 flex h-6 w-6 cursor-pointer items-center justify-center rounded-full  border-secondary bg-white text-gray transition-all duration-300 ease-in-out hover:border-secondary hover:bg-secondary hover:text-white 2xl:my-2 2xl:h-12 2xl:w-12">
+const Instance = ({ Icon, href }: { Icon: IconType; href: string }) => (
+  <a
+    href={href}
+    target="_blank"
+    rel="noopener noreferrer"
+    className="my-1 flex h-6 w-6 cursor-pointer items-center justify-center rounded-full  border-secondary bg-white text-gray transition-all duration-300 ease-in-out hover:border-secondary hover:bg-secondary hover:text-white 2xl:my-2 2xl:h-12 2xl:w-12"
+  >
     <Icon className="h-3 w-3 text-current 2xl:h-6 2xl:w-6" />
-  </div>
+  </a>
 );
 
-// TODO: real social media
 const SocialMedia = () => (
-  <div className="absolute bottom-0 left-1 flex flex-col items-center justify-end md:bottom-8 md:left-8">
-    <Instance Icon={BsTwitter} />
-    <Instance Icon={FaFacebookF} />
-    <Instance Icon={BsInstagram} />
+  <div className="absolute bottom-0 left-1 z-50 flex flex-col items-center justify-end md:bottom-8 md:left-8">
+    <Instance
+      Icon={FaLinkedinIn}
+      href="https://www.linkedin.com/in/adam-rajmuller/"
+    />
+    <Instance Icon={FaGithub} href="https://github.com/AdamReinmuller" />
+    <Instance Icon={BsTwitter} href="https://twitter.com/adamrraj" />
   </div>
 );
 
