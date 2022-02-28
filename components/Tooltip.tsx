a6420b30b2aaa8abe0fc6ede5ccbf56897e98255
adamRajmuller <rajfta@gmail.com>
2022-02-28 15:20:37 +0100

    polygon tooltip

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -11,7 +11,13 @@ const Tooltip = ({ children, show }: TooltipProps) => {
   }
 
   return (
-    <div className="absolute top-0 left-1/2 min-w-[220px] max-w-xs -translate-x-1/2 translate-y-[-110%] items-center justify-center rounded bg-zinc-700 p-4 leading-normal text-white opacity-100 shadow-[0px_0px_25px_rgba(0,0,0,0.1)] 2xl:max-w-lg">
+    <div
+      style={{
+        clipPath:
+          'polygon(0% 0%, 100% 0%, 100% 93%, 55% 93%, 50% 100%, 45% 93%, 0 93%)',
+      }}
+      className="absolute top-0 left-1/2 min-w-[220px] max-w-xs -translate-x-1/2 -translate-y-full items-center justify-center bg-zinc-700 p-4 pb-6 leading-normal text-white opacity-100 shadow-[0px_0px_25px_rgba(0,0,0,0.1)] 2xl:max-w-lg"
+    >
       {children}
     </div>
   );
